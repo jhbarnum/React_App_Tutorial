d6a803a5d88d1aed7615e9bf7758dc139f0cc154
Jared <jhbarnum@gmail.com>
2018-11-09 06:06:52 -0500

    event handling

--- src/App.js
+++ src/App.js
@@ -3,14 +3,28 @@ import './App.css';
 import Person from './Person/Person.js';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Jared', age: 41 },
+      { name: 'Laura', age: 34 },
+      { name: 'Georgia', age: 3 },
+      { name: 'Abel', age: .5 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log('Clicked');
+  }
+
   render() {
     return (
       <div className="App">
         <h1>hello world</h1>
-        <Person name="Jared" age="41"> I like coding</Person>
-        <Person name="Laura" age="34"/>
-        <Person name="Georgia" age="3"/>
-        <Person name="Abel" age="6 months"/> 
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}> I like coding</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <Person name={this.state.persons[3].name} age={this.state.persons[3].age}/> 
       </div>
     );
   }
