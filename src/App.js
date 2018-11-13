fecf7252346b85c0bb05d528fbced71dc549eb70
Jared <jhbarnum@gmail.com>
2018-11-13 06:14:48 -0500

    bind

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person.js';
+import Cakes from './Cakes.js/Cakes.js';
 
 class App extends Component {
   state = {
+    cakes: [
+      {name: 'chocolate', age: '5 days'}
+    ],
     persons: [
       { name: 'Jared', age: 41 },
       { name: 'Laura', age: 34 },
@@ -12,19 +16,39 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
-    console.log('Clicked');
+  switchNameHandler = (newName) => {
+//console.log('Clicked');
+    //this.state.persons[0].name = 'Barnum';
+    this.setState( {
+      persons: [
+    { name: newName, age: 40 },
+    { name: 'Laura', age: 33 },
+    { name: 'Georgia', age: 2 },
+    { name: 'Abel', age: .5 }
+      ]
+    })
   }
 
   render() {
     return (
       <div className="App">
         <h1>hello world</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}> I like coding</Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-        <Person name={this.state.persons[3].name} age={this.state.persons[3].age}/> 
+        <button onClick={() => this.switchNameHandler('JB')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}> I like coding</Person>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'J-Rock')}/>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}/>
+        <Person 
+          name={this.state.persons[3].name} 
+          age={this.state.persons[3].age}/> 
+        <Cakes 
+          name={this.state.cakes[0].name}/>
       </div>
     );
   }
